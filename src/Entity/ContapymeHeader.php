d53f691f8891b54b9098968579affbc9674e1420
Juan Miguel <jmgalzate@outlook.com>
2023-11-11 22:54:22 -0500

    Removing ORM annotations for non needed tables.

--- src/Entity/ContapymeHeader.php
+++ src/Entity/ContapymeHeader.php
@@ -1,99 +1,12 @@
 <?php
 
-/**
- * TODO: add the rest of the Entities for ContapymeBody
- */
-
 namespace App\Entity;
-
-use App\Repository\ContapymeHeaderRepository;
-use Doctrine\ORM\Mapping as ORM;
-
-#[ORM\Entity(repositoryClass: ContapymeHeaderRepository::class)]
 class ContapymeHeader
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
     private ?int $id = null;
-
-    #[ORM\Column]
     private ?bool $resultado = null;
-
-    #[ORM\Column(length: 255)]
     private ?string $imensaje = null;
-
-    #[ORM\Column(length: 255)]
     private ?string $mensaje = null;
-
-    #[ORM\Column(length: 255)]
     private ?string $tiempo = null;
-
-    #[ORM\Column(length: 255)]
     private ?string $version = null;
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function isResultado(): ?bool
-    {
-        return $this->resultado;
-    }
-
-    public function setResultado(bool $resultado): static
-    {
-        $this->resultado = $resultado;
-
-        return $this;
-    }
-
-    public function getImensaje(): ?string
-    {
-        return $this->imensaje;
-    }
-
-    public function setImensaje(string $imensaje): static
-    {
-        $this->imensaje = $imensaje;
-
-        return $this;
-    }
-
-    public function getMensaje(): ?string
-    {
-        return $this->mensaje;
-    }
-
-    public function setMensaje(string $mensaje): static
-    {
-        $this->mensaje = $mensaje;
-
-        return $this;
-    }
-
-    public function getTiempo(): ?string
-    {
-        return $this->tiempo;
-    }
-
-    public function setTiempo(string $tiempo): static
-    {
-        $this->tiempo = $tiempo;
-
-        return $this;
-    }
-
-    public function getVersion(): ?string
-    {
-        return $this->version;
-    }
-
-    public function setVersion(string $version): static
-    {
-        $this->version = $version;
-
-        return $this;
-    }
 }
--- src/Entity/OrderInvoiceSettlement.php
+++ src/Entity/OrderInvoiceSettlement.php
@@ -2,79 +2,10 @@
 
 namespace App\Entity;
 
-use App\Repository\OrderInvoiceSettlementRepository;
-use Doctrine\ORM\Mapping as ORM;
-
-#[ORM\Entity(repositoryClass: OrderInvoiceSettlementRepository::class)]
 class OrderInvoiceSettlement
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
-
-    #[ORM\Column(length: 255)]
     private ?string $parcial = null;
-
-    #[ORM\Column(length: 255)]
     private ?string $descuento = null;
-
-    #[ORM\Column(length: 255)]
     private ?string $iva = null;
-
-    #[ORM\Column(length: 255)]
     private ?string $total = null;
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getParcial(): ?string
-    {
-        return $this->parcial;
-    }
-
-    public function setParcial(string $parcial): static
-    {
-        $this->parcial = $parcial;
-
-        return $this;
-    }
-
-    public function getDescuento(): ?string
-    {
-        return $this->descuento;
-    }
-
-    public function setDescuento(string $descuento): static
-    {
-        $this->descuento = $descuento;
-
-        return $this;
-    }
-
-    public function getIva(): ?string
-    {
-        return $this->iva;
-    }
-
-    public function setIva(string $iva): static
-    {
-        $this->iva = $iva;
-
-        return $this;
-    }
-
-    public function getTotal(): ?string
-    {
-        return $this->total;
-    }
-
-    public function setTotal(string $total): static
-    {
-        $this->total = $total;
-
-        return $this;
-    }
 }
