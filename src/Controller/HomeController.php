0feadcdda506f04b88e047980a5ecd2f73dc1b3b
jmgalzate <jmgalzate@outlook.com>
2023-12-02 22:46:36 -0500

    The home page shows the products.quantity and prepare a 'template' for orders summary

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,21 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use App\Service\ProductService;
 
 class HomeController extends AbstractController
 {
 
     public function __construct(
+        private readonly ProductService $productService
     ) {
     }
 
     #[Route('/', name: 'homepage')]
     public function index(): Response
     {
+        
         return $this->render('components/Home.html.twig', [
             'title' => 'Bienvenid@ al Portal de Despachos',
             'dispatchButton' => 'Nuevo despacho',
-            'loadProductsButton' => 'Cargar productos',
+            'totalProducts' => $this->productService->totalProductsInDB()
         ]);
     }
 }
