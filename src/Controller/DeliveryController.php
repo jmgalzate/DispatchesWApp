df49ad41466dc04d85e01efe7257dd228f028eff
jmgalzate <jmgalzate@outlook.com>
2023-12-06 10:30:27 -0500

    Delivery. Controller, Entity, pages, first commit for definition in Order Handling

--- src/Controller/DeliveryController.php
+++ src/Controller/DeliveryController.php
@@ -2,17 +2,64 @@
 
 namespace App\Controller;
 
+use App\Service\OrderService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DeliveryController extends AbstractController
 {
+    
+    public function __construct(
+        private readonly OrderService $orderService,
+    ) {}
     #[Route('/delivery', name: 'home_delivery')]
     public function index(): Response
     {
-        return $this->render('components/Delivery.html.twig', [
+        return $this->render('components/DeliveryHome.html.twig', [
             'title' => 'Despachar Orden'
         ]);
     }
+    
+    #[Route('/delivery/{orderNumber}', name: 'delivery_order')]
+    public function deliveryOrder($orderNumber): JsonResponse
+    {
+        $order = $this->orderService->process($orderNumber);
+        $orderLoad = $this->orderService->load($orderNumber);
+        
+        if($orderLoad->getStatusCode() != 200) {
+            return new JsonResponse([
+                'status' => 'error',
+                'message' => 'Error al cargar la orden'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        
+        $orderData = $orderLoad->getContent();
+        $orderArray = json_decode($orderData, true);
+        
+        return new JsonResponse([
+            'status' => 'ok',
+            'Order' => $orderArray,
+        ], Response::HTTP_OK);
+    }
+    
+    #[Route('/delivery/{orderNumber}/close', name: 'delivery_order_close')]
+    public function deliveryOrderClose($orderNumber): JsonResponse
+    {
+        return new JsonResponse([
+            'status' => 'ok',
+            'message' => 'Orden cerrada correctamente'
+        ], Response::HTTP_OK);
+    }
+    
+    #[Route('/delivery/{orderNumber}/save', name: 'delivery_order_save')]
+    public function deliveryOrderSave($orderNumber): JsonResponse
+    {
+        return new JsonResponse([
+            'status' => 'ok',
+            'message' => 'Orden guardada correctamente'
+        ], Response::HTTP_OK);
+    }
+
 }
---
+++ src/Entity/Delivery.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DeliveryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+class Delivery
+{
+    #[ ORM\Column(type: 'integer') ]
+    #[ ORM\Id ]
+    #[ ORM\GeneratedValue ]
+    private ?int $id = null;
+
+    #[ORM\Column(name: 'orderNumber', type: 'integer')]
+    private ?int $orderNumber = null;
+    
+    #[ORM\Column(name: 'customerName', type: 'string', length: 255)]
+    private ?string $customerName = null;
+    
+    #[ORM\Column(name: 'customerId', type: 'bigint')]
+    private ?int $customerId = null;
+    
+    #[ORM\Column(name: 'createdAt', type: 'datetime')]
+    private ?\DateTimeInterface $createdAt = null;
+    
+    #[ORM\Column(name: 'totalRequested', type: 'integer')]
+    private ?int $totalRequested = null;
+    
+    #[ORM\Column(name: 'totalDispatched', type: 'integer')]
+    private ?int $totalDispatched = null;
+    
+    #[ORM\Column(name: 'efficiency', type: 'decimal', precision: 1, scale: 4)]
+    private ?float $efficiency = null;
+    
+    #[ORM\Column(name: 'productsList', type: 'text')]
+    private ?string $productsList = null;
+    
+    public function getId (): ?int {
+        return $this->id;
+    }
+    
+    public function getOrderNumber (): ?int {
+        return $this->orderNumber;
+    }
+    
+    public function setOrderNumber (int $orderNumber): self {
+        $this->orderNumber = $orderNumber;
+        
+        return $this;
+    }
+    
+    public function getCustomerName (): ?string {
+        return $this->customerName;
+    }
+    
+    public function setCustomerName (string $customerName): self {
+        $this->customerName = $customerName;
+        
+        return $this;
+    }
+    
+    public function getCustomerId (): ?int {
+        return $this->customerId;
+    }
+    
+    public function setCustomerId (int $customerId): self {
+        $this->customerId = $customerId;
+        
+        return $this;
+    }
+    
+    public function getCreatedAt (): ?\DateTimeInterface {
+        return $this->createdAt;
+    }
+    
+    public function setCreatedAt (\DateTimeInterface $createdAt): self {
+        $this->createdAt = $createdAt;
+        
+        return $this;
+    }
+    
+    public function getTotalRequested (): ?int {
+        return $this->totalRequested;
+    }
+    
+    public function setTotalRequested (int $totalRequested): self {
+        $this->totalRequested = $totalRequested;
+        
+        return $this;
+    }
+    
+    public function getTotalDispatched (): ?int {
+        return $this->totalDispatched;
+    }
+    
+    public function setTotalDispatched (int $totalDispatched): self {
+        $this->totalDispatched = $totalDispatched;
+        
+        return $this;
+    }
+    
+    public function getEfficiency (): ?float {
+        return $this->efficiency;
+    }
+    
+    public function setEfficiency (float $efficiency): self {
+        $this->efficiency = $efficiency;
+        
+        return $this;
+    }
+    
+    public function getProductsList (): ?string {
+        return $this->productsList;
+    }
+    
+    public function setProductsList (string $productsList): self {
+        $this->productsList = $productsList;
+        
+        return $this;
+    }
+}
