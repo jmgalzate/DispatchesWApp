31eecb593a7ad1252792786726fc0090904922b0
jmgalzate <jmgalzate@outlook.com>
2023-12-08 08:58:36 -0500

    Delivery pages an' redirections are created

--- src/Controller/DeliveryController.php
+++ src/Controller/DeliveryController.php
@@ -2,9 +2,7 @@
 
 namespace App\Controller;
 
-use App\Service\OrderService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,54 +10,23 @@ class DeliveryController extends AbstractController
 {
     
     public function __construct(
-        private readonly OrderService $orderService,
     ) {}
     #[Route('/delivery', name: 'home_delivery')]
     public function index(): Response
     {
-        return $this->render('components/DeliveryHome.html.twig', [
+        return $this->render('components/Delivery/Home.html.twig', [
             'title' => 'Despachar Orden'
         ]);
     }
     
     #[Route('/delivery/{orderNumber}', name: 'delivery_order')]
-    public function deliveryOrder($orderNumber): JsonResponse
+    public function deliveryOrder($orderNumber): Response
     {
-        $order = $this->orderService->process($orderNumber);
-        $orderLoad = $this->orderService->load($orderNumber);
-        
-        if($orderLoad->getStatusCode() != 200) {
-            return new JsonResponse([
-                'status' => 'error',
-                'message' => 'Error al cargar la orden'
-            ], Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
-        
-        $orderData = $orderLoad->getContent();
-        $orderArray = json_decode($orderData, true);
-        
-        return new JsonResponse([
-            'status' => 'ok',
-            'Order' => $orderArray,
-        ], Response::HTTP_OK);
-    }
-    
-    #[Route('/delivery/{orderNumber}/close', name: 'delivery_order_close')]
-    public function deliveryOrderClose($orderNumber): JsonResponse
-    {
-        return new JsonResponse([
-            'status' => 'ok',
-            'message' => 'Orden cerrada correctamente'
-        ], Response::HTTP_OK);
-    }
-    
-    #[Route('/delivery/{orderNumber}/save', name: 'delivery_order_save')]
-    public function deliveryOrderSave($orderNumber): JsonResponse
-    {
-        return new JsonResponse([
-            'status' => 'ok',
-            'message' => 'Orden guardada correctamente'
-        ], Response::HTTP_OK);
+        return $this->render('components/Delivery/Order.html.twig', [
+            'Order' => $orderNumber,
+            'CustomerName' => 'Juan',
+            'CustomerNIT' => '90077886655',
+        ]);
     }
 
 }
