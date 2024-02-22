00f64f287471bdcfe2c5983e95555fe11d1fb020
jmgalzate <jmgalzate@outlook.com>
2024-02-21 21:40:20 -0500

    Method to check if the dispatch already exist is implemented in the GET order

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -6,7 +6,9 @@ use App\Entity\Delivery;
 use App\Entity\Order;
 use App\Service\ContapymeService;
 use App\Service\ProductService;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,7 +17,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class OrderController extends AbstractController
 {
-  public function __construct(
+  public function __construct (
     private readonly ContapymeService       $contapymeService,
     private readonly ProductService         $productService,
     private readonly EntityManagerInterface $entityManager
@@ -23,16 +25,11 @@ class OrderController extends AbstractController
   }
 
   #[Route('api/get/order={orderNumber}', name: 'getOrder', methods: ['GET'])]
-  public function getOrder(Request $request, int $orderNumber): JsonResponse
-  {
+  public function getOrder (Request $request, int $orderNumber): JsonResponse {
 
     if (!$request->headers->has('Accept') || $request->headers->get('Accept') !== 'application/json') {
       return new JsonResponse('Unauthorized', Response::HTTP_UNAUTHORIZED);
     }
-
-    /**
-     * TODO: revisar si la orden ya existe en la tabla Despachos, y si ya ha sido despachada previamente. Si ya se despachó, informar que la orden ya se despachó, de lo contrario, retornar el objeto Despacho para que se pueda actualizar.
-     */
 
     try {
 
@@ -43,14 +40,14 @@ class OrderController extends AbstractController
       );
 
       if ($orderRequest['code'] !== Response::HTTP_OK)
-        throw new \Exception($orderRequest['body']);
+        throw new Exception($orderRequest['body']);
 
       /** 2. Deserialize the order*/
       $order = Order::fromArray(orderNumber: $orderNumber, orderData: $orderRequest['body']['datos']);
 
       /** 3. Validate if there are products in the Order*/
       if (empty($order->getListaproductos()))
-        throw new \Exception('La orden ' . $orderNumber . ' no tiene productos para despachar.');
+        throw new Exception('La orden ' . $orderNumber . ' no tiene productos para despachar.');
 
 
       /** 4. Check if the order is processed to unprocess it */
@@ -63,21 +60,45 @@ class OrderController extends AbstractController
         );
 
         if ($orderUnprocessed['code'] !== Response::HTTP_OK)
-          throw new \Exception($orderUnprocessed['body']);
+          throw new Exception($orderUnprocessed['body']);
 
         $order->setIprocess(2); // 2 = Unprocessed
       }
 
       /** 6. The Order is recorded in the DB */
       $this->entityManager->getRepository(Order::class)->save($order);
-    } catch (\Exception $e) {
+    } catch (Exception $e) {
       return new JsonResponse([
         'code' => Response::HTTP_INTERNAL_SERVER_ERROR,
         'message' => $e->getMessage()
       ]);
     }
 
-    /** 7. Set the products to Dispatch */
+    /**
+     * 7. Check if the delivery is already in the database and if it is dispatched.
+     */
+
+    $deliveryRepository = $this->entityManager->getRepository(Delivery::class);
+    $delivery = $deliveryRepository->findOneBy(['orderNumber' => $orderNumber]);
+
+    if ($delivery) {
+
+      if ($delivery->getIsDispatched() === false) {
+        $jsonResponse = new JsonResponse($delivery->jsonSerialize());
+        $jsonResponse->setStatusCode(Response::HTTP_OK);
+        return $jsonResponse;
+      }
+
+      $jsonResponse = new JsonResponse([
+        'code' => Response::HTTP_OK,
+        'message' => 'La orden ya fue despachada.' //TODO: create arrow function for process again the order. 
+      ]);
+
+      $jsonResponse->setStatusCode(Response::HTTP_OK);
+      return $jsonResponse;
+    }
+
+    /** 8. Set the products to Dispatch */
     $productsToDispatch = $this->productService->setProductsLists($order->getListaproductos());
 
     $totalRequested = 0;
@@ -86,11 +107,11 @@ class OrderController extends AbstractController
       $totalRequested += $product->getRequestedQuantity();
     }
 
-    /** 8. Set and record the Delivery request */
+    /** 9. Set and record the Delivery request */
     $delivery = (new Delivery())
       ->setOrderNumber($orderNumber)
       ->setCustomerId($order->getDatosprincipales()->init)
-      ->setCreatedAt(new \DateTime())
+      ->setCreatedAt(new DateTime())
       ->setTotalRequested($totalRequested)
       ->setTotalDispatched(0)
       ->setEfficiency(0)
@@ -99,7 +120,7 @@ class OrderController extends AbstractController
 
     $delivery = $this->entityManager->getRepository(Delivery::class)->save($delivery, true);
 
-    /** 9. The Delivery object is serialized and returned */
+    /** 10. The Delivery object is serialized and returned */
 
     $jsonResponse = new JsonResponse($delivery->jsonSerialize());
     $jsonResponse->setStatusCode(Response::HTTP_OK);
@@ -111,8 +132,7 @@ class OrderController extends AbstractController
    * This method will receive the order that must be updated in Contapyme and the Delivery object to update the efficiency and the dispatched products.
    */
   #[Route('api/update/delivery', name: 'updateOrder', methods: ['PUT'])]
-  public function updateOrder(Request $request): JsonResponse
-  {
+  public function updateOrder (Request $request): JsonResponse {
 
     if (!$request->headers->has('Accept') || $request->headers->get('Accept') !== 'application/json') {
       return new JsonResponse('Unauthorized', Response::HTTP_UNAUTHORIZED);
@@ -127,12 +147,12 @@ class OrderController extends AbstractController
         ->setId($deliveryData['id'])
         ->setOrderNumber($deliveryData['orderNumber'])
         ->setCustomerId($deliveryData['customerId'])
-        ->setCreatedAt(new \DateTime())
+        ->setCreatedAt(new DateTime())
         ->setTotalRequested($deliveryData['totalRequested'])
         ->setTotalDispatched($deliveryData['totalDispatched'])
         ->setEfficiency($deliveryData['efficiency'])
         ->setProductsList($deliveryData['productsList']
-        ->setIsDispatched(true));
+          ->setIsDispatched(true));
 
       $this->entityManager->getRepository(Delivery::class)->save($delivery, true);
 
@@ -179,7 +199,7 @@ class OrderController extends AbstractController
       );
 
       if ($orderSaved['code'] !== Response::HTTP_OK)
-        throw new \Exception('Falló el intento de "Guardar" la orden en Contapyme; por favor valide los logs para obtener más detalles de la transacción.');
+        throw new Exception('Falló el intento de "Guardar" la orden en Contapyme; por favor valide los logs para obtener más detalles de la transacción.');
 
       $orderTaxes = $this->contapymeService->agentAction(
         actionId: 5,
@@ -188,7 +208,7 @@ class OrderController extends AbstractController
       );
 
       if ($orderTaxes['code'] !== Response::HTTP_OK)
-        throw new \Exception('Falló el intento de "Calcular los Impuestos" de la orden en Contapyme; por favor valide los logs para obtener más detalles de la transacción.');
+        throw new Exception('Falló el intento de "Calcular los Impuestos" de la orden en Contapyme; por favor valide los logs para obtener más detalles de la transacción.');
 
 
       /** 2. Try to process the Order */
@@ -198,7 +218,7 @@ class OrderController extends AbstractController
       );
 
       if ($orderProcessed['code'] !== Response::HTTP_OK)
-        throw new \Exception('Falló el intento de "Procesar" la orden en Contapyme; por favor verifique y procésela manualmente.');
+        throw new Exception('Falló el intento de "Procesar" la orden en Contapyme; por favor verifique y procésela manualmente.');
 
       /** 3. Confirm the Order is closed in this API without changes in Contapyme. */
       $jsonResponse = new JsonResponse([
@@ -209,7 +229,7 @@ class OrderController extends AbstractController
       $jsonResponse->setStatusCode(Response::HTTP_OK);
 
       return $jsonResponse;
-    } catch (\Exception $e) {
+    } catch (Exception $e) {
       return new JsonResponse([
         'code' => Response::HTTP_NOT_MODIFIED,
         'message' => $e->getMessage()
@@ -221,8 +241,7 @@ class OrderController extends AbstractController
    * This method will receive the order that must not be updated in Contapyme but the Delivery object is updated.
    */
   #[Route('api/dispatch/delivery', name: 'closeAndDispatch', methods: ['PUT'])]
-  public function closeAndDispatch(Request $request): JsonResponse
-  {
+  public function closeAndDispatch (Request $request): JsonResponse {
     try {
       /** Setting the received Delivery */
 
@@ -231,15 +250,14 @@ class OrderController extends AbstractController
         ->setId($deliveryData['id'])
         ->setOrderNumber($deliveryData['orderNumber'])
         ->setCustomerId($deliveryData['customerId'])
-        ->setCreatedAt(new \DateTime())
+        ->setCreatedAt(new DateTime())
         ->setTotalRequested($deliveryData['totalRequested'])
-        
         ->setTotalDispatched($deliveryData['totalDispatched'])
         ->setEfficiency($deliveryData['efficiency'])
         ->setProductsList($deliveryData['productsList'])
         ->setIsDispatched(true);
 
-      $this->entityManager->getRepository(Delivery::class)->update($delivery, true);
+      $this->entityManager->getRepository(Delivery::class)->update($delivery);
 
 
       /** 2. Try to process the Order */
@@ -249,7 +267,7 @@ class OrderController extends AbstractController
       );
 
       if ($orderProcessed['code'] !== Response::HTTP_OK)
-        throw new \Exception('Falló el intento de "Procesar" la orden en Contapyme; por favor verifique y procésela manualmente.');
+        throw new Exception('Falló el intento de "Procesar" la orden en Contapyme; por favor verifique y procésela manualmente.');
 
       /** 3. Confirm the Order is closed in this API without changes in Contapyme. */
       $jsonResponse = new JsonResponse([
@@ -257,7 +275,7 @@ class OrderController extends AbstractController
         'message' => 'El despacho ha sido registrado sin guardar los cambios en Contapyme.'
       ]);
 
-    } catch (\Exception $e) {
+    } catch (Exception $e) {
       $jsonResponse = new JsonResponse([
         'code' => Response::HTTP_INTERNAL_SERVER_ERROR,
         'message' => $e->getMessage()
@@ -267,12 +285,11 @@ class OrderController extends AbstractController
     return $jsonResponse;
   }
 
-    /**
+  /**
    * This method will receive the order that must not be updated in Contapyme and the Dispatch is set to false, as the order is not dispatched.
    */
   #[Route('api/cancel/delivery', name: 'closeWithoutDispatch', methods: ['DELETE'])]
-  public function closeWithoutDispatch(Request $request): JsonResponse
-  {
+  public function closeWithoutDispatch (Request $request): JsonResponse {
 
     if (!$request->headers->has('Accept') || $request->headers->get('Accept') !== 'application/json') {
       return new JsonResponse('Unauthorized', Response::HTTP_UNAUTHORIZED);
@@ -291,7 +308,7 @@ class OrderController extends AbstractController
       );
 
       if ($orderProcessed['code'] !== Response::HTTP_OK)
-        throw new \Exception('Falló el intento de "Procesar" la orden en Contapyme; por favor verifique y procésela manualmente.');
+        throw new Exception('Falló el intento de "Procesar" la orden en Contapyme; por favor verifique y procésela manualmente.');
 
       /** 3. Confirm the Order is closed in this API without changes in Contapyme. */
       $jsonResponse = new JsonResponse([
@@ -302,7 +319,7 @@ class OrderController extends AbstractController
       $jsonResponse->setStatusCode(Response::HTTP_OK);
 
       return $jsonResponse;
-    } catch (\Exception $e) {
+    } catch (Exception $e) {
       return new JsonResponse([
         'code' => Response::HTTP_NOT_MODIFIED,
         'message' => $e->getMessage()
