55092eeddbba5bcc41b498a3ada0241884649d02
jmgalzate <jmgalzate@outlook.com>
2024-02-24 10:47:33 -0500

    Doctrine column names updated

--- src/Entity/Log.php
+++ src/Entity/Log.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\Entity;
 
@@ -8,51 +8,51 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: LogRepository::class)]
 #[ORM\Table(name: 'log')]
 
-class Log 
+class Log
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column(name: 'id', type: 'integer')]
-    private ?int $id;
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column(name: 'id', type: 'integer')]
+  private ?int $id;
 
-    //0. Error, 1. Warning, 2. Info
-    #[ORM\Column(name: 'logType', type: 'integer')]
-    private ?int $logType = null;
+  //0. Error, 1. Warning, 2. Info
+  #[ORM\Column(name: 'log_type', type: 'integer')]
+  private ?int $logType = null;
 
-    #[ORM\Column(name: 'logDetails', type: 'text')]
-    private ?string $logDetails = null;
+  #[ORM\Column(name: 'log_details', type: 'json')] //Changed type to 'json'
+  private $logDetails = null;
 
-    #[ORM\Column(name: 'createdAt', type: 'datetime')]
-    private ?\DateTimeInterface $createdAt = null;
+  #[ORM\Column(name: 'created_at', type: 'datetime')]
+  private ?\DateTimeInterface $createdAt = null;
 
-    public function getId(): ?int {
-        return $this->id;
-    }
+  public function getId(): ?int {
+    return $this->id;
+  }
 
-    public function getLogType(): ?int {
-        return $this->logType;
-    }
+  public function getLogType(): ?int {
+    return $this->logType;
+  }
 
-    public function setLogType(int $logType): self {
-        $this->logType = $logType;
-        return $this;
-    }
+  public function setLogType(int $logType): self {
+    $this->logType = $logType;
+    return $this;
+  }
 
-    public function getLogDetails(): ?string {
-        return $this->logDetails;
-    }
+  public function getLogDetails() { //Removed return type
+    return $this->logDetails;
+  }
 
-    public function setLogDetails(string $logDetails): self {
-        $this->logDetails = $logDetails;
-        return $this;
-    }
+  public function setLogDetails($logDetails): self { //Removed parameter type
+    $this->logDetails = $logDetails;
+    return $this;
+  }
 
-    public function getCreatedAt(): ?\DateTimeInterface {
-        return $this->createdAt;
-    }
+  public function getCreatedAt(): ?\DateTimeInterface {
+    return $this->createdAt;
+  }
 
-    public function setCreatedAt(\DateTimeInterface $createdAt): self {
-        $this->createdAt = $createdAt;
-        return $this;
-    }
+  public function setCreatedAt(\DateTimeInterface $createdAt): self {
+    $this->createdAt = $createdAt;
+    return $this;
+  }
 }
