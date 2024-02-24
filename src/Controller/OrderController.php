cf0057526420cd63701b52f76978c0b4f7e780b0
jmgalzate <jmgalzate@outlook.com>
2024-02-24 12:35:56 -0500

    Log methods where created for recording the relevant information in the log table in the database

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -6,6 +6,7 @@ use App\Entity\Delivery;
 use App\Entity\Order;
 use App\Service\ContapymeService;
 use App\Service\ProductService;
+use App\Entity\Log;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
@@ -17,11 +18,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class OrderController extends AbstractController
 {
+  private $loggerRepository;
+  
   public function __construct (
     private readonly ContapymeService       $contapymeService,
     private readonly ProductService         $productService,
     private readonly EntityManagerInterface $entityManager
   ) {
+    $this->loggerRepository = $this->entityManager->getRepository(Log::class);
   }
 
   #[Route('api/get/order={orderNumber}', name: 'getOrder', methods: ['GET'])]
@@ -30,6 +34,11 @@ class OrderController extends AbstractController
     if (!$request->headers->has('Accept') || $request->headers->get('Accept') !== 'application/json') {
       return new JsonResponse('Unauthorized', Response::HTTP_UNAUTHORIZED);
     }
+
+    /**
+     * This function will be used to unprocess the order in Contapyme to avoid calling the same code twice in this
+     * method.
+     */
 
     $unprocess = function ($orderNumber) {
       $orderUnprocessed = $this->contapymeService->agentAction(
@@ -52,23 +61,47 @@ class OrderController extends AbstractController
 
       if ($delivery) {
 
+        $this->loggerRepository->save(
+          (new Log())
+            ->setLogType(1)
+            ->setLogDetails(json_encode([
+              'action' => 'LOAD',
+              'details' => 'The order ' . $orderNumber . ' was already downloaded from Contapyme and will not be download again.'
+            ]))
+            ->setCreatedAt(new \DateTime()),
+          true
+        );
+
         if ($delivery->getIsDispatched() === false) {
 
           $unprocess($orderNumber);
 
           $jsonResponse = new JsonResponse($delivery->jsonSerialize());
           $jsonResponse->setStatusCode(Response::HTTP_OK);
-          return $jsonResponse;
+          
         } else {
+          
           $jsonResponse = new JsonResponse([
             'code' => Response::HTTP_ALREADY_REPORTED,
             'message' => 'La orden ya fue despachada.' //TODO: create arrow function for process again the order. 
           ]);
 
           $jsonResponse->setStatusCode(Response::HTTP_ALREADY_REPORTED);
-          return $jsonResponse;
         }
+        return $jsonResponse;
       } else {
+
+        $this->loggerRepository->save(
+          (new Log())
+            ->setLogType(2)
+            ->setLogDetails(json_encode([
+              'action' => 'LOAD',
+              'details' => 'Downloading the order ' . $orderNumber . ' from Contapyme.'
+            ]))
+            ->setCreatedAt(new \DateTime()),
+          true
+        );
+        
         /** 1. Try to get the order */
         $orderRequest = $this->contapymeService->agentAction(
           actionId: 3,
@@ -103,14 +136,36 @@ class OrderController extends AbstractController
 
         /** 6. The Order is recorded in the DB */
         $this->entityManager->getRepository(Order::class)->save($order);
+
+        $this->loggerRepository->save(
+          (new Log())
+            ->setLogType(1)
+            ->setLogDetails(json_encode([
+              'action' => 'LOAD',
+              'details' => 'The order ' . $orderNumber . ' has been downloaded and recorded in the database.'
+            ]))
+            ->setCreatedAt(new \DateTime()),
+          true
+        );
       }
     } catch (Exception $e) {
+
+      $this->loggerRepository->save(
+        (new Log())
+          ->setLogType(0)
+          ->setLogDetails(json_encode([
+            'action' => 'LOAD',
+            'details' => 'An error happened while trying to download the order ' . $orderNumber . ' from Contapyme. ' . $e->getMessage()
+          ]))
+          ->setCreatedAt(new \DateTime()),
+        true
+      );
+      
       return new JsonResponse([
         'code' => Response::HTTP_INTERNAL_SERVER_ERROR,
         'message' => $e->getMessage()
       ]);
     }
-
 
     /** 8. Set the products to Dispatch */
     $productsToDispatch = $this->productService->setProductsLists($order->getListaproductos());
@@ -157,6 +212,18 @@ class OrderController extends AbstractController
       /** Setting the received Delivery */
 
       $deliveryData = json_decode($request->getContent(), true);
+
+      $this->loggerRepository->save(
+        (new Log())
+          ->setLogType(2)
+          ->setLogDetails(json_encode([
+            'action' => 'SAVE',
+            'details' => 'Starting process to update the order ' . $deliveryData['orderNumber']. ' in Contapyme.'
+          ]))
+          ->setCreatedAt(new \DateTime()),
+        true
+      );
+      
       $delivery = (new Delivery())
         ->setId($deliveryData['id'])
         ->setOrderNumber($deliveryData['orderNumber'])
@@ -237,10 +304,34 @@ class OrderController extends AbstractController
         'message' => 'La orden ha sido guardada en Contapyme y procesada correctamente.'
       ]);
 
+
+      $this->loggerRepository->save(
+        (new Log())
+          ->setLogType(2)
+          ->setLogDetails(json_encode([
+            'action' => 'SAVE',
+            'details' => 'Ended the process to update the order ' . $deliveryData['orderNumber']. ' in Contapyme.'
+          ]))
+          ->setCreatedAt(new \DateTime()),
+        true
+      );
+      
       $jsonResponse->setStatusCode(Response::HTTP_OK);
 
       return $jsonResponse;
     } catch (Exception $e) {
+
+      $this->loggerRepository->save(
+        (new Log())
+          ->setLogType(0)
+          ->setLogDetails(json_encode([
+            'action' => 'SAVE',
+            'details' => 'The process to save the order in Contapyme failed.' . $e->getMessage()
+          ]))
+          ->setCreatedAt(new \DateTime()),
+        true
+      );
+      
       return new JsonResponse([
         'code' => Response::HTTP_NOT_MODIFIED,
         'message' => $e->getMessage()
@@ -257,6 +348,19 @@ class OrderController extends AbstractController
       /** Setting the received Delivery */
 
       $deliveryData = json_decode($request->getContent(), true);
+
+      $this->loggerRepository->save(
+        (new Log())
+          ->setLogType(2)
+          ->setLogDetails(json_encode([
+            'action' => 'Dispatch',
+            'details' => 'Starting process to save delivery without updating Contapyme for the order ' . 
+              $deliveryData['orderNumber']. '.'
+          ]))
+          ->setCreatedAt(new \DateTime()),
+        true
+      );
+      
       $delivery = (new Delivery())
         ->setId($deliveryData['id'])
         ->setOrderNumber($deliveryData['orderNumber'])
@@ -269,8 +373,7 @@ class OrderController extends AbstractController
         ->setIsDispatched(true);
 
       $this->entityManager->getRepository(Delivery::class)->update($delivery);
-
-
+      
       /** 2. Try to process the Order */
       $orderProcessed = $this->contapymeService->agentAction(
         actionId: 6,
@@ -280,6 +383,18 @@ class OrderController extends AbstractController
       if ($orderProcessed['code'] !== Response::HTTP_OK)
         throw new Exception('Falló el intento de "Procesar" la orden en Contapyme; por favor verifique y procésela manualmente.');
 
+
+      $this->loggerRepository->save(
+        (new Log())
+          ->setLogType(2)
+          ->setLogDetails(json_encode([
+            'action' => 'SAVE',
+            'details' => 'Ended the process to update the order ' . $deliveryData['orderNumber']. ' in Contapyme.'
+          ]))
+          ->setCreatedAt(new \DateTime()),
+        true
+      );
+      
       /** 3. Confirm the Order is closed in this API without changes in Contapyme. */
       $jsonResponse = new JsonResponse([
         'code' => Response::HTTP_OK,
@@ -287,6 +402,18 @@ class OrderController extends AbstractController
       ]);
 
     } catch (Exception $e) {
+
+      $this->loggerRepository->save(
+        (new Log())
+          ->setLogType(0)
+          ->setLogDetails(json_encode([
+            'action' => 'SAVE',
+            'details' => 'An error happened while trying to save the delivery without updating Contapyme for the order' . $e->getMessage()
+          ]))
+          ->setCreatedAt(new \DateTime()),
+        true
+      );
+      
       $jsonResponse = new JsonResponse([
         'code' => Response::HTTP_INTERNAL_SERVER_ERROR,
         'message' => $e->getMessage()
@@ -312,6 +439,17 @@ class OrderController extends AbstractController
       $order = json_decode($request->getContent(), true);
       $orderNumber = $order['orderNumber'];
 
+      $this->loggerRepository->save(
+        (new Log())
+          ->setLogType(2)
+          ->setLogDetails(json_encode([
+            'action' => 'CANCEL',
+            'details' => 'Starting process to close the order ' . $orderNumber['orderNumber']. ' without dispatch or updates in Contapyme.'
+          ]))
+          ->setCreatedAt(new \DateTime()),
+        true
+      );
+
       /** 2. Try to process the Order */
       $orderProcessed = $this->contapymeService->agentAction(
         actionId: 6,
@@ -327,10 +465,33 @@ class OrderController extends AbstractController
         'message' => 'El despacho se ha cancelado y la orden se cerró en Contapyme sin guardar cambios.'
       ]);
 
+      $this->loggerRepository->save(
+        (new Log())
+          ->setLogType(2)
+          ->setLogDetails(json_encode([
+            'action' => 'CANCEL',
+            'details' => 'Ended process to close the order ' . $orderNumber['orderNumber']. ' without dispatch or updates in Contapyme.'
+          ]))
+          ->setCreatedAt(new \DateTime()),
+        true
+      );
+
       $jsonResponse->setStatusCode(Response::HTTP_OK);
 
       return $jsonResponse;
     } catch (Exception $e) {
+
+      $this->loggerRepository->save(
+        (new Log())
+          ->setLogType(0)
+          ->setLogDetails(json_encode([
+            'action' => 'CANCEL',
+            'details' => 'An error happened while trying to close the order without dispatch or updates in Contapyme. ' . $e->getMessage()
+          ]))
+          ->setCreatedAt(new \DateTime()),
+        true
+      );
+      
       return new JsonResponse([
         'code' => Response::HTTP_NOT_MODIFIED,
         'message' => $e->getMessage()
